2358c0da2b8404e439a68f604acf0b0394542aa3
kidusabdula <107987568+kidusabdula@users.noreply.github.com>
2025-04-16 00:08:34 +0300

    Clerk Middleware Update

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/product(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/product(.*)", "/details(.*)", "/cart(.*)", "/checkout(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
